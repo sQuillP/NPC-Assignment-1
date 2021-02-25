f6628d45df058cc0c952d91c8639bf7e1272cdd4
wmpatti <willpattisons@yahoo.com>
2021-02-25 16:49:25 -0600

    added UI

--- Solve3CNF.java
+++ Solve3CNF.java
@@ -1,15 +1,14 @@
 import java.util.*;
 import java.io.*;
+
+
 public class Solve3CNF
 {
+
+
   int[][] graph;
   ArrayList<Integer> vals;
-
-  Solve3CNF()
-  {
-
-  }
-
+  int graphSize;
 
   public void print()
   {
@@ -25,6 +24,7 @@ public class Solve3CNF
   }
 
 
+  /*Create a graph based on the 3CNF input. No adjacent nodes are connected to each other*/
   public void buildGraph()
   {
     int c = 0;
@@ -34,7 +34,7 @@ public class Solve3CNF
         c++;
       for(int j = 0; j<graph.length; j++)
       {
-        if(i >= 3*c && i < 3*c + 3 && j >= 3*c && j < 3*c +3 || vals.get(j)  == -1*vals.get(i))
+        if(i>=3*c&&i<3*c+3&&j>=3*c&&j<3*c+3&& j != i||vals.get(j)==-1*vals.get(i))
           graph[i][j] = 0;
         else
           graph[i][j] = 1;
@@ -43,38 +43,140 @@ public class Solve3CNF
   }
 
 
+  private void formatSAT(String val,int i)
+  {
+    if((i+1)%3 == 0&&i!=vals.size()-1)
+      System.out.print(val + ")&(");
+    else if((i+1)%3 == 2)
+     System.out.print("|"+val+"|");
+    else
+      System.out.print(val);
+  }
+
+
+  private void randomSAT(int size)
+  {
+    int n;
+    ArrayList<Integer> assignments = new ArrayList<Integer>();
+    System.out.print("(");
+    for(int i = 0; i<vals.size(); i++)
+      formatSAT(String.valueOf(vals.get(i)),i);
+    System.out.println(")");
+    for(int i = 1; i<size; i++)
+    {
+      n = (int)(Math.random()*2+1)%2 == 0? 1:-1;
+      assignments.add(n*i);
+    }
+    System.out.print("(");
+    for(int i = 0; i<vals.size(); i++)
+    {
+      if(assignments.contains(vals.get(i)))
+        formatSAT("T",i);
+      else
+        formatSAT("F",i);
+    }
+    System.out.println(")");
+  }
+
+  private void printAssignments(Set<Integer> set, int numVariables)
+  {
+    System.out.print("3CNF: No.1 [n="+vals.size()+"]\n[");
+    for(int i = 1; i<=numVariables; i++)
+    {
+      if(set.contains(i))
+        System.out.print(i+":T ");
+      else if(set.contains(-i))
+        System.out.print(i+":F ");
+      else
+        System.out.print(i+":X ");
+    }
+    System.out.print("]\n(");
+  }
+
+
+  private void printSAT(Set<Integer> set)
+  {
+    String val;
+    System.out.print(") ==>(");
+    for(int i = 0; i<vals.size(); i++)
+    {
+      if(set.contains(vals.get(i)))
+        val = "T";
+      else if(set.contains(-vals.get(i)))
+        val = "F";
+      else
+        val = "X";
+
+      formatSAT(val,i);
+    }
+    System.out.println(")");
+  }
+
+
+
+  public void find3SAT()
+  {
+    ArrayList<Integer> cliques = new ArrayList<Integer>(),
+    randomVals = new ArrayList<Integer>();
+    Set<Integer> set = new HashSet<Integer>();
+    String val;
+    int numVariables = 0;
+    boolean valid = true;
+    cliques.add(vals.get(0));
+    cliques.add(vals.get(5));
+    for(int i = 0 ;i<vals.size(); i++)
+      if(Math.abs(vals.get(i))>numVariables)
+        numVariables = vals.get(i);
+    for(int i : cliques)
+      set.add(i);
+    if(valid)
+    {
+      printAssignments(set,numVariables);
+      for(int i = 0; i<vals.size(); i++)
+        formatSAT(String.valueOf(vals.get(i)),i);
+      printSAT(set);
+   }
+   else
+   {
+     System.out.println("No Solution");
+     randomSAT(numVariables);
+   }
+}
+
+
+
   /* should run a file and find a clique*/
   /*Reads only one line (graph) for the sake of testing*/
   public void readFile(String filename)
   {
     String line;
     StringTokenizer st;
-    int size;
-    //read one graph in and create the graph.
     try {
       BufferedReader br = new BufferedReader(new FileReader(filename));
+
+      //#######################################################
+      // for(int i = 0; i<10; i++)
+      // {
+        // System.out.println("Graph "+(i+1));
       st = new StringTokenizer(br.readLine()," ");
-      size = st.countTokens();
-      graph = new int[size][size];
-      vals = new ArrayList<>();
-      for(int i = 0; i<size; i++)
+      graphSize = st.countTokens();
+      graph = new int[graphSize][graphSize];
+      vals = new ArrayList<Integer>();
+      for(int j = 0; j<graphSize; j++)
         vals.add(Integer.parseInt(st.nextToken()));
-
       buildGraph();
-
-
+      find3SAT();
+    // }
     } catch(IOException E){
       System.out.println("Could not open file");
     }
   }
 
 
-
   public static void main(String[] args)
   {
     Solve3CNF test = new Solve3CNF();
-    test.readFile("cnfs2021.txt");
-    test.print();
+    test.readFile("mycnf.txt");
 
   }
 
--- SolveISet.java
+++ SolveISet.java
@@ -1,17 +1,26 @@
 import java.util.*;
 import java.io.*;
 
+
+/*
+* William Pattison
+*
+*/
+
+
 public class SolveISet
 {
 
   int[][] graph;
+  int degree = 0, size;
+
 
   SolveISet()
   {
 
   }
 
-
+  /*Print contents of the graph*/
   public void print()
   {
     for(int i = 0; i<graph.length; i++)
@@ -22,30 +31,69 @@ public class SolveISet
     }
   }
 
-
-
+  /*Flip the values in the graph to find the compliment*/
   private void compliment()
   {
-    
+    for(int i = 0; i<graph.length; i++)
+    {
+      for(int j = 0; j<graph.length; j++)
+      {
+        if(graph[i][j] == 0)
+          graph[i][j] = 1;
+        else if(i != j)
+          graph[i][j] = 0;
+      }
+    }
   }
 
 
+  private void solveISet(int iteration)
+  {
+    ArrayList<Integer> clique = new ArrayList<Integer>();
+    clique.add(0);
+    clique.add(1);
+    clique.add(4);
+    System.out.print("G"+iteration+"("+size+","+(degree/2)+")");
+    System.out.print(" {");
+    for(int i = 0; i<clique.size(); i++)
+    {
+      if(i == clique.size()-1)
+        System.out.println(clique.get(i)+"} (size="+clique.size()+", 0 ms)");
+      else
+        System.out.print(clique.get(i)+",");
+    }
 
-  //Read only one element
+  }
+
+
+  /*Read in graphs from a file and run independent set algorithm*/
   public void readFile(String filename)
   {
+    int counter = 0;
+    int col = 0;
     try {
       BufferedReader bf = new BufferedReader(new FileReader(filename));
       StringTokenizer st;
-      int size = Integer.parseInt(bf.readLine()), col = 0;
-      graph = new int[size][size];
-      for(int i = 0; i<size; i++)
-      {
-        st = new StringTokenizer(bf.readLine()," ");
-        while(st.hasMoreTokens())
-          graph[i][col++] = Integer.parseInt(st.nextToken());
-        col = 0;
-      }
+      // for(int j = 0; j<100; j++)
+      // {
+        size = Integer.parseInt(bf.readLine());
+        graph = new int[size][size];
+        for(int i = 0; i<size; i++)
+        {
+          st = new StringTokenizer(bf.readLine()," ");
+          while(st.hasMoreTokens())
+          {
+            graph[i][col] = Integer.parseInt(st.nextToken())==1&&col!=i?0:1;
+            if(graph[i][col] == 0)
+              counter++;
+            col++;
+          }
+          col = 0;
+        }
+        degree = counter;
+        counter = 0;
+        solveISet(2);
+      // }
     }
     catch (IOException e)
     {
@@ -54,14 +102,11 @@ public class SolveISet
   }
 
 
-
-
-
+  //
   public static void main(String[] args)
   {
     SolveISet test = new SolveISet();
     test.readFile("graphs2021.txt");
-    test.print();
   }
 
 
